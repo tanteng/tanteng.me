333084fce9fb2ce71d750ba92b12653dceac5e69
tuntun <tuntun@juanpi.com>
2016-02-15 17:48:44 +0800

    首页封面样式调整

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,85 +16,87 @@
     <!-- Custom styles for this template -->
     <link href="/assets/styles/cover.css" rel="stylesheet">
 
-    <!--[if lt IE 9]><script src="/assets/js/ie8-responsive-file-warning.js'"></script><![endif]-->
+    <!--[if lt IE 9]>
+    <script src="/assets/js/ie8-responsive-file-warning.js'"></script><![endif]-->
     <script src="/assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
 
-  <body>
+<body>
 
-    <div class="site-wrapper">
+<div class="site-wrapper">
 
-      <div class="site-wrapper-inner">
+    <div class="site-wrapper-inner">
 
         <div class="cover-container">
 
-          <div class="masthead clearfix inner">
-            <nav class="navbar navbar-inverse">
-              <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="/">tanteng.me</a>
+            <div class="masthead clearfix inner">
+                <nav class="navbar navbar-inverse">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                    data-target="#navbar" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="/">tanteng.me</a>
+                        </div>
+
+                        <div class="collapse navbar-collapse navbar-right" id="navbar">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="/">Home</a></li>
+                                <li><a href="http://www.tantengvip.com" target="_blank">Blog</a></li>
+                                <li><a href="/resume">Resume</a></li>
+                                <li><a href="/contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+            <div class="inner cover">
+                <div style="margin: auto; width: 75%;">
+                    <div class="row">
+                        <img class="img-circle" src="/assets/images/avatar.jpg" alt="Generic placeholder image"
+                             width="140" height="140">
+
+                        <h2>谈腾</h2>
+
+                        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh
+                            ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                            Praesent commodo cursus magna.</p>
+
+                        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                    </div>
                 </div>
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse navbar-right" id="navbar">
-                  <ul class="nav navbar-nav">
-                    <li class="active"><a href="/">Home</a></li>
-                    <li><a href="http://www.tantengvip.com" target="_blank">Blog</a></li>
-                    <li><a href="/resume">Resume</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                  </ul>
-                </div><!-- /.navbar-collapse -->
-              </div><!-- /.container-fluid -->
-            </nav>
-          </div>
-
-          <div class="inner cover">
-            <div style="margin: auto; width: 75%;">
-              <div class="row">
-                <div class="col-l55g-4">
-                  <img class="img-circle" src="/assets/images/avatar.jpg" alt="Generic placeholder image" width="140" height="140">
-                  <h2>谈腾</h2>
-                  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                  <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-              </div>
+                <div class="mastfoot">
+                    <div class="inner">
+                        <address>
+                            <strong>谈腾</strong><br>
+                            深圳市南山科技园<br>
+                            <a href="mailto:tanteng@gmail.com">tanteng@gmail.com</a>
+                        </address>
+                    </div>
+                </div>
             </div>
 
-            <div class="mastfoot">
-              <div class="inner">
-                <address>
-                  <strong>谈腾</strong><br>
-                  深圳市南山科技园<br>
-                  <a href="mailto:tanteng@gmail.com">tanteng@gmail.com</a>
-                </address>
-              </div>
-            </div>
         </div>
 
-      </div>
-
     </div>
+</div>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-    <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
 </html>
 
