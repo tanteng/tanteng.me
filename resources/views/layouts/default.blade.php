7850c65407e2f65afa17fe6c2dabc15d8f6b044b
tanteng <tanteng@gmail.com>
2016-04-10 13:19:24 +0800

    applicable-device

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="applicable-device" content="pc,mobile">
     <meta name="description" content="@yield('description')">
     <link href="//cdn.tanteng.me" rel="dns-prefetch">
     <link href="//cdn.tanteng.me/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
