f67c2147b323220e6f5ac22161f66c26eb0d9b07
tanteng <tanteng@gmail.com>
2016-05-01 21:11:45 +0800

    update

--- app/Http/Controllers/EnglishController.php
+++ app/Http/Controllers/EnglishController.php
@@ -21,12 +21,13 @@ class EnglishController extends Controller
         $detail = English::where('slug', $slug)->firstOrFail();
         $nextSlug = English::where('id', '>', $detail['id'])->min('slug');
         $prevSlug = English::where('id', '<', $detail['id'])->max('slug');
+        $latest = English::latest('id')->take(10)->get(['slug', 'seo_title']);
         $phrase = $detail['phrase'];
         $seoTitle = $detail['seo_title'] . '_英语_tanteng.me';
         $description = $detail['description'];
         $content = Markdown::convertToHtml($detail['content']);
         $canonical = 'http://english.tanteng.me/how-to-say/' . $slug;
-        $compact = compact('navFlag', 'slug', 'phrase', 'seoTitle', 'description', 'content', 'canonical', 'nextSlug', 'prevSlug');
+        $compact = compact('navFlag', 'slug', 'phrase', 'seoTitle', 'description', 'content', 'canonical', 'latest', 'nextSlug', 'prevSlug');
         return view('english.how-to-say', $compact);
     }
 }
--- resources/views/english/how-to-say.blade.php
+++ resources/views/english/how-to-say.blade.php
@@ -1,38 +1,40 @@
-@extends('layouts.default')
+@extends('layouts.english')
 
 @section('title'){{ $seoTitle }}@endsection
 @section('description'){{ $description }}@endsection
 @section('canonical'){{ $canonical }}@endsection
 
 @section('content')
-<div class="container">
-    <ol class="breadcrumb">
-        <li><a href="{{ route('english.index') }}">English</a></li>
-        <li><a href="{{ route('how-to-say.index') }}">英文怎么说</a></li>
-        <li class="active"><a href="{{ route('how-to-say.detail', ['slug'=>$slug]) }}">{{ $phrase }}</a></li>
-    </ol>
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="{{ route('how-to-say.index') }}">英文怎么说</a></li>
+            <li class="active"><a href="{{ route('how-to-say.detail', ['slug'=>$slug]) }}">{{ $phrase }}</a></li>
+        </ol>
 
-    <div class="row" id="english">
-        <div class="col-lg-8 col-md-8">
-            <div class="content">
-                {!! $content !!}
+        <div class="row" id="english">
+            <div class="col-lg-8 col-md-8">
+                <div class="content">
+                    {!! $content !!}
+                </div>
+                <nav>
+                    <ul class="pager">
+                        @if($prevSlug)
+                            <li><a href="{{ route('how-to-say.detail', ['slug'=>$prevSlug]) }}" rel="prev">上一篇</a>
+                            </li>@endif
+                        @if($nextSlug)
+                            <li><a href="{{ route('how-to-say.detail', ['slug'=>$nextSlug]) }}" rel="next">下一篇</a>
+                            </li>@endif
+                    </ul>
+                </nav>
             </div>
-            <nav>
-                <ul class="pager">
-                    @if($prevSlug)<li><a href="{{ route('how-to-say.detail', ['slug'=>$prevSlug]) }}">上一篇</a></li>@endif
-                    @if($nextSlug)<li><a href="{{ route('how-to-say.detail', ['slug'=>$nextSlug]) }}">下一篇</a></li>@endif
-                </ul>
-            </nav>
-        </div>
-        <div class="col-lg-4 col-md-4">
-            <div class="list-group">
-                <a href="#" class="list-group-item">Cras justo odio</a>
-                <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-                <a href="#" class="list-group-item">Morbi leo risus</a>
-                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                <a href="#" class="list-group-item">Vestibulum at eros</a>
+            <div class="col-lg-4 col-md-4">
+                <div class="list-group">
+                    @foreach($latest as $list)
+                        <a href="{{ route('how-to-say.detail', ['slug'=>$list->slug]) }}"
+                           class="list-group-item">{{ $list->seo_title }}</a>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
