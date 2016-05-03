24d67530c586fef66cf4f8b1f14e5278ba4efd02
tanteng <tanteng@gmail.com>
2016-05-03 22:13:31 +0800

    缓存提高性能

--- app/Http/Controllers/EnglishController.php
+++ app/Http/Controllers/EnglishController.php
@@ -14,7 +14,7 @@ class EnglishController extends Controller
     public function index()
     {
         $navFlag = $this->navFlag;
-        $latest = English::latest('id')->take(20)->get(['slug', 'seo_title']);
+        $latest = $this->latest(40);
         $seo['description'] = '英文怎么说';
         return view('english.index', compact('latest', 'seo', 'navFlag'));
     }
@@ -23,21 +23,43 @@ class EnglishController extends Controller
     {
         $navFlag = $this->navFlag;
         $detail = English::where('slug', $slug)->firstOrFail();
-        $nextId = English::where('id', '>', $detail['id'])->min('id');
-        $prevId = English::where('id', '<', $detail['id'])->max('id');
-        $nextSlug = $prevSlug = '';
-        if($nextId){
-            $nextSlug = English::where('id', $nextId)->first(['slug']);
-        }
-        if($prevId){
-            $prevSlug = English::where('id', $prevId)->first(['slug']);
-        }
-        $latest = English::latest('id')->take(10)->get(['id', 'slug', 'seo_title']);
+        $prevSlug = $this->getSlug($detail['id'], 'prev');
+        $nextSlug = $this->getSlug($detail['id'], 'next');
+        $latest = $this->latest(10);
         $phrase = $detail['phrase'];
         $seoTitle = $detail['seo_title'] . '_英语_tanteng.me';
         $description = $detail['description'];
         $content = Markdown::convertToHtml($detail['content']);
         $compact = compact('navFlag', 'slug', 'phrase', 'seoTitle', 'description', 'content', 'canonical', 'latest', 'prevSlug', 'nextSlug');
         return view('english.how-to-say', $compact);
+    }
+
+    //缓存最新文章
+    public function latest($nums)
+    {
+        $key = 'english.latest.articles.' . $nums;
+        $latest = Cache::remember($key, 30, function () use ($nums) {
+            return English::latest('id')->take($nums)->paginate(20, ['id', 'slug', 'seo_title']);
+        });
+        return $latest;
+    }
+
+    //缓存上一篇下一篇
+    public function getSlug($id, $prevOrnext = 'prev')
+    {
+        $slugId = $slug = '';
+        if ($prevOrnext == 'prev') {
+            $slugId = English::where('id', '<', $id)->max('id');
+        } elseif ($prevOrnext == 'next') {
+            $slugId = English::where('id', '>', $id)->min('id');
+        }
+
+        if ($slugId) {
+            $key = 'english.article.' . $id . '.' . $prevOrnext;
+            $slug = Cache::remember($key, 30, function () use ($slugId) {
+                return English::where('id', $slugId)->first(['slug']);
+            });
+        }
+        return $slug;
     }
 }
