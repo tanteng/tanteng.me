2883997f479506c249106961a0c48184cef4a8bd
tanteng <tanteng@gmail.com>
2016-05-12 00:03:54 +0800

    auth

--- app/Http/Controllers/Admin/TravelController.php
+++ app/Http/Controllers/Admin/TravelController.php
@@ -17,6 +17,7 @@ class TravelController extends Controller
 {
     public function __construct(Destination $destination)
     {
+        $this->middleware('auth:admin');
         $this->destination = $destination;
         $this->allDestination = $this->destination->getAll();
     }
