f5ea27f7a1e7a0e2ebb6610c636acc197880023b
root <tanteng@gmail.com>
2016-05-19 12:49:18 +0800

    update

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.2.31 on 2016-05-16.
+ * Generated for Laravel 5.2.31 on 2016-05-19.
  *
  * @author Barry vd. Heuvel <barryvdh@gmail.com>
  * @see https://github.com/barryvdh/laravel-ide-helper
@@ -7788,7 +7788,7 @@ namespace {
          * Order of precedence: PATH (routing placeholders or custom attributes), GET, BODY
          *
          * @param string $key the key
-         * @param mixed $default the default value if the parameter key does not exist
+         * @param mixed $default the default value
          * @return mixed 
          * @static 
          */
@@ -8240,7 +8240,7 @@ namespace {
          * Here is the process to determine the format:
          * 
          *  * format defined by the user (with setRequestFormat())
-         *  * _format request attribute
+         *  * _format request parameter
          *  * $default
          *
          * @param string $default The default format
