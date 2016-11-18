66f14a16ebf29f0e5efee497ed8adf61fab72da2
谈腾 <tanteng@xunlei.com>
2016-11-18 21:59:11 +0800

    test command

--- app/Console/Commands/MyTest.php
+++ app/Console/Commands/MyTest.php
@@ -37,11 +37,31 @@ class MyTest extends Command
      */
     public function handle()
     {
-        $this->comment('begin');
-        $bankCardNo = '3445590948739874';
-        $bankCardNoEncrypt = encrypt($bankCardNo);
-        $this->comment('encrypt:' . $bankCardNoEncrypt);
-        $decrypt = decrypt($bankCardNoEncrypt);
-        $this->comment('decrypt:'.$decrypt);
+        $array = [1, 5, 6, 7, 3, 98, 334, 2, 23, 54];
+        $result = $this->bubbleSort($array);
+        $this->comment(json_encode($result));
+    }
+
+    /**
+     * 冒泡排序
+     * @param $array
+     * @return mixed
+     */
+    public function bubbleSort($array)
+    {
+        info('bubble', $array);
+        $length = count($array);
+        info('bubble', array('length' => $length));
+        for ($i = 0; $i < $length; $i++) {
+            for ($j = 0; $j < $length - 1 - $i; $j++) {
+                if ($array[$j] > $array[$j + 1]) {
+                    $temp = $array[$j];
+                    $array[$j + 1] = $array[$j];
+                    $array[$j] = $temp;
+                }
+            }
+        }
+        info('bubble', array('result' => $array));
+        return $array;
     }
 }
